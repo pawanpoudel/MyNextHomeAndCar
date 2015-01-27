f5cd526bf2af4c92ce45537f2f6a9809de3f85d6
Pawan Poudel <pawan.poudel@gmail.com>
2015-01-26 23:00:47 -0500

    Create a new Xcode project

---
+++ MyNextHomeAndCar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyNextHomeAndCar
+//
+//  Created by Pawan Poudel on 1/26/15.
+//  Copyright (c) 2015 Pawan Poudel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
